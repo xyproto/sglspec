08a687410a2ce4850c534253840012fdebdab51f
Alexander Fet Rødseth <rodseth@gmail.com>
2023-04-11 10:09:46 +0200

    Add more pure functions

--- pure.go
+++ pure.go
@@ -72,6 +72,171 @@ func LowPassFilter(data []float64, cutoff float64, sampleRate int) []float64 {
 
 // HighPassFilter applies a high-pass filter to the provided data.
 func HighPassFilter(data []float64, cutoff float64, sampleRate int) []float64 {
-	// TO BE IMPLEMENTED
-	return nil
+	alpha := 1 / (1 + (1 / (2 * math.Pi * cutoff / float64(sampleRate))))
+	filteredData := make([]float64, len(data))
+	filteredData[0] = data[0]
+
+	for i := 1; i < len(data); i++ {
+		filteredData[i] = alpha * (filteredData[i-1] + data[i] - data[i-1])
+	}
+
+	return filteredData
+}
+
+// Distortion applies distortion effect to the provided data.
+func Distortion(data []float64, drive float64, tone float64, mix float64) []float64 {
+	distortedData := make([]float64, len(data))
+
+	for i := 0; i < len(data); i++ {
+		distortedData[i] = (1 - mix) * data[i]
+		distortedData[i] += mix * (2 / math.Pi * math.Atan(drive*data[i]))
+	}
+
+	// Apply tone control (low-pass filter)
+	distortedData = LowPassFilter(distortedData, tone*20000, 44100)
+
+	return distortedData
+}
+
+// Reverb applies reverb effect to the provided data.
+func Reverb(data []float64, mix float64, decayTime float64, sampleRate int) []float64 {
+	// Comb filter constants
+	combCount := 8
+	combDelay := []int{1617, 1557, 1491, 1422, 1277, 1116, 897, 778}
+	combGain := make([]float64, combCount)
+	for i := 0; i < combCount; i++ {
+		combGain[i] = math.Pow(10, -3*float64(combDelay[i])/float64(sampleRate)/decayTime)
+	}
+
+	// All-pass filter constants
+	allPassCount := 4
+	allPassDelay := []int{225, 556, 441, 341}
+	allPassGain := 0.5
+
+	// Apply comb filters
+	combData := make([][]float64, combCount)
+	for i := 0; i < combCount; i++ {
+		combData[i] = make([]float64, len(data)+combDelay[i])
+		for j := 0; j < len(data); j++ {
+			combData[i][j+combDelay[i]] = data[j] + combGain[i]*combData[i][j]
+		}
+	}
+
+	// Mix comb filters
+	mixedData := make([]float64, len(data))
+	for i := 0; i < len(data); i++ {
+		for j := 0; j < combCount; j++ {
+			mixedData[i] += combData[j][i+combDelay[j]]
+		}
+	}
+
+	// Apply all-pass filters
+	allPassData := make([][]float64, allPassCount)
+	for i := 0; i < allPassCount; i++ {
+		allPassData[i] = make([]float64, len(data)+allPassDelay[i])
+		for j := 0; j < len(data); j++ {
+			allPassData[i][j+allPassDelay[i]] = mixedData[j] + allPassGain*allPassData[i][j]
+			mixedData[j] = allPassData[i][j] - allPassGain*mixedData[j]
+		}
+	}
+
+	// Apply mix
+	for i := 0; i < len(data); i++ {
+		data[i] = (1-mix)*data[i] + mix*mixedData[i]
+	}
+
+	return data
+}
+
+// BandPassFilter applies a band-pass filter to the provided data with the specified low and high cutoff frequencies.
+func BandPassFilter(data []float64, lowCutoff float64, highCutoff float64, sampleRate int) []float64 {
+	lowPassedData := LowPassFilter(data, highCutoff, sampleRate)
+	bandPassedData := HighPassFilter(lowPassedData, lowCutoff, sampleRate)
+	return bandPassedData
+}
+
+// Chorus adds a chorus effect to the provided data with the specified rate, depth, and mix.
+func Chorus(data []float64, rate float64, depth float64, mix float64, sampleRate int) []float64 {
+	// This is a simple implementation of a chorus effect. You may need to adjust the parameters for your specific use case.
+	chData := make([]float64, len(data))
+	for i := 0; i < len(data); i++ {
+		offset := int(depth * math.Sin(2*math.Pi*rate*float64(i)/float64(sampleRate)))
+		if i+offset < len(data) {
+			chData[i] = data[i]*(1-mix) + data[i+offset]*mix
+		} else {
+			chData[i] = data[i]
+		}
+	}
+	return chData
+}
+
+// Phaser adds a phaser effect to the provided data with the specified rate, depth, feedback, and mix.
+func Phaser(data []float64, rate float64, depth float64, feedback float64, mix float64, sampleRate int) []float64 {
+	// This is a simple implementation of a phaser effect. You may need to adjust the parameters for your specific use case.
+	phData := make([]float64, len(data))
+	feedbackData := make([]float64, len(data))
+	for i := 0; i < len(data); i++ {
+		offset := int(depth * math.Sin(2*math.Pi*rate*float64(i)/float64(sampleRate)))
+		if i+offset < len(data) {
+			phData[i] = data[i]*(1-mix) + (data[i+offset]+feedbackData[i])*mix
+			feedbackData[i] = phData[i] * feedback
+		} else {
+			phData[i] = data[i]
+		}
+	}
+	return phData
+}
+
+// Flanger adds a flanger effect to the provided data with the specified rate, depth, feedback, and mix.
+func Flanger(data []float64, rate float64, depth float64, feedback float64, mix float64, sampleRate int) []float64 {
+	// This is a simple implementation of a flanger effect. You may need to adjust the parameters for your specific use case.
+	flData := make([]float64, len(data))
+	feedbackData := make([]float64, len(data))
+	for i := 0; i < len(data); i++ {
+		offset := int(depth * math.Sin(2*math.Pi*rate*float64(i)/float64(sampleRate)))
+		if i+offset < len(data) {
+			flData[i] = data[i]*(1-mix) + (data[i+offset]+feedbackData[i])*mix
+			feedbackData[i] = flData[i] * feedback
+		} else {
+			flData[i] = data[i]
+		}
+	}
+	return flData
+}
+
+// Delay adds a delay effect to the provided data with the specified delay time, feedback, and mix.
+func Delay(data []float64, delayTime float64, feedback float64, mix float64, sampleRate int) []float64 {
+	delayData := make([]float64, len(data))
+	delaySamples := int(delayTime * float64(sampleRate) / 1000)
+
+	for i := 0; i < len(data); i++ {
+		if i-delaySamples >= 0 {
+			delayData[i] = data[i]*(1-mix) + (data[i-delaySamples]+delayData[i-delaySamples]*feedback)*mix
+		} else {
+			delayData[i] = data[i]
+		}
+	}
+
+	return delayData
+}
+
+// Normalize normalizes the provided data to the specified amplitude.
+func Normalize(data []float64, amplitude float64) []float64 {
+	normalizedData := make([]float64, len(data))
+	max := 0.0
+
+	for _, sample := range data {
+		if math.Abs(sample) > max {
+			max = math.Abs(sample)
+		}
+	}
+
+	if max > 0 {
+		scale := amplitude / max
+		for i := range data {
+			normalizedData[i] = data[i] * scale
+		}
+	}
+
+	return normalizedData
 }
