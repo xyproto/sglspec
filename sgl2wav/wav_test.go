cc28dc7e8425ab149d116cc8ea8fc0372f36ebd2
Alexander Fet Rødseth <rodseth@gmail.com>
2023-04-12 00:07:06 +0200

    Add another test

---
+++ sgl2wav/wav_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestWriteWAVFile(t *testing.T) {
+	filename := "test.wav"
+	duration := 1 * time.Second
+	amplitude := 0.8
+	sampleRate := 44100
+
+	// Generate white noise
+	audioData := GenerateWhiteNoise(duration, amplitude, sampleRate)
+
+	// Write WAV file
+	err := WriteWAVFile(filename, audioData, sampleRate)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(filename)
+
+	// Read WAV file
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Check if file contains any audio data at all
+	if len(data) < 44 { // WAV header is 44 bytes
+		t.Errorf("WAV file %s contains no audio data", filename)
+	}
+}
