304a4f2ebdbce96bedc89c289f5d6f3c697379c3
Alexander Fet Rødseth <rodseth@gmail.com>
2023-04-11 16:48:35 +0200

    Remove a shell script used for testing

--- sgl2wav/output.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-grep -n -r "GenerateWhiteNoise" *.go
-grep -n -r "GeneratePinkNoise" *.go
-grep -n -r "GenerateBrownianNoise" *.go
-grep -n -r "EffectData" *.go
-grep -n -r "listener.data" *.go
-grep -n -r "ctx.GetChild" *.go
