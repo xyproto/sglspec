fa08f6a76fca4ab2c19342912656b21a477ddde9
Alexander Fet Rødseth <rodseth@gmail.com>
2023-04-12 00:10:32 +0200

    Add a test

---
+++ sgl2wav/sglfunctions_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestGenerateAudioData(t *testing.T) {
+	// Create a listener with a sine wave oscillator and amplitude envelope
+	listener := &SGLCustomListener{
+		oscillators: []OscillatorData{
+			{
+				Waveform:  "sine",
+				Frequency: 440,
+				Mix:       1,
+			},
+		},
+		envelopes: []EnvelopeData{
+			{
+				AttackTime:   time.Millisecond * 10,
+				DecayTime:    time.Millisecond * 10,
+				SustainLevel: 0.5,
+				ReleaseTime:  time.Millisecond * 10,
+			},
+		},
+		effects: []EffectData{},
+	}
+	// Generate audio data
+	sampleRate := 44100
+	audioData := GenerateAudioData(listener, sampleRate)
+
+	// Check that audio data has been generated and is not all zeroes
+	hasData := false
+	for _, sample := range audioData {
+		if sample != 0 {
+			hasData = true
+			break
+		}
+	}
+	if !hasData {
+		t.Error("Generated audio data is all zeroes")
+	}
+}
